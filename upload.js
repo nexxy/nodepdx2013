b6160d1fa61841088238803ae9c90d961d07ae53
Emily Rose <emily@contactvibe.com>
2013-05-16 07:14:00 -0700

    Fixed silly error.

--- upload.js
+++ upload.js
@@ -2,6 +2,8 @@ var fs = require('fs')
 var request = require('request')
 
 var r = request.post('http://' + process.argv[2] + ':9090/track')
-r.on('error', "OH GOD IT BROKE")
+r.on('error', function() {
+  console.log("OH GOD IT BROKE")
+})
 var form = r.form()
 form.append('track', fs.createReadStream(process.argv[3]))
